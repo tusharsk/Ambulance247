5dae0ed34dd2a452bc0af35f1aea697b94483710
Anubhav <anubhavaron000051@gmail.com>
2018-04-05 03:55:25 +0530

    testing 1

--- app/src/main/java/com/example/tusharsk/ambulance247/MainActivity.java
+++ app/src/main/java/com/example/tusharsk/ambulance247/MainActivity.java
@@ -15,7 +15,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    void fun()
+    {}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
