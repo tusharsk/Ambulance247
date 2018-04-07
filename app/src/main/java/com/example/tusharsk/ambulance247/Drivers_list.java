7984e6367ee6f58b123184a30452552d433dc690
Anubhav <anubhavaron000051@gmail.com>
2018-04-08 03:48:22 +0530

    slide up panel

---
+++ app/src/main/java/com/example/tusharsk/ambulance247/Drivers_list.java
@@ -0,0 +1,13 @@
+package com.example.tusharsk.ambulance247;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Drivers_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivers_list);
+    }
+}
